6e56152d390ec6e8205174fe057fa326dd6adf71
Aadarsh A <aadarsh.ram@gmail.com>
2023-01-15 21:42:15 +0530

    feat: Complete JD db operations

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 import uvicorn
 import aiofiles
 import tempfile
@@ -10,10 +11,19 @@ from fastapi.exceptions import HTTPException
 # Import custom modules
 from curl_helper import SiteCurler
 from pdf_parser import parse_pdf
+from db_operations import Database
 
 # Import all environment variables
 load_dotenv()
 
+# Postgres database variables
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+
+# Initialize database
+db = Database(host=db_host, user=db_user, password=db_password, database=db_name)
 app = FastAPI()
 
 # Allow anyone to call the API from their own apps
@@ -26,9 +36,34 @@ app.add_middleware(
     expose_headers=["*"],
 )
 
+@app.on_event("startup")
+async def startup():
+    await db.dbs_connect()
+    await db.create_tables()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.dbs_disconnect()
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def hello():
     return {"message": "Hello user! Tip: open /docs or /redoc for documentation"}
+
+@app.get("/jds")
+async def get_all_jds():
+    """
+    Get all the job descriptions from the database
+    """
+    result = await db.get_all_jds()
+    return (result)
+
+@app.get("/jd/{jd_id}")
+async def get_jd(jd_id: int):
+    """
+    Get a specific job description from the database
+    """
+    result = await db.get_jd(jd_id)
+    return (result)
 
 @app.get("/candidate-profiles/indeed")
 async def get_candidate_profiles(response: Response, keywords: str, offset: Optional[int] = 0):
@@ -57,9 +92,13 @@ async def handle_pdf_upload(request: Request, response: Response, file: UploadFi
                 await out_file.write(content)
             
             content = parse_pdf(filepath) # JD Content
+            keywords = "test" # Keywords
+            await db.insert_jd(content, keywords)
+
+            return {"message": "File uploaded successfully"}
             
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail="File upload unsuccessful")
 
 if __name__ == '__main__':
     uvicorn.run("app:app", reload=True)
